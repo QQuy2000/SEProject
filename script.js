3539ac08255cffb8d4206aa3bb80a8faf9eb0788
datphamcode295 <vandatpro2000@gmail.com>
2021-11-05 20:33:08 +0700

    news

--- script.js
+++ script.js
@@ -85,6 +85,34 @@ const  handleMenuTab3 = async ()=>{
     a.innerHTML = html;          
 };
 
+const  handleNews = async ()=>{
+    const response = await fetch('https://617bd868d842cf001711c0fe.mockapi.io/news');
+    const myJson = await response.json();
+    let newJson = [];
+    if(myJson.length>6)
+        newJson = myJson.slice(0,6);
+    const html = newJson.map((items) =>{
+        // console.log(items);
+    return `
+    <div class="news-card">
+                        <img src="./assets/images/news1.jpg" alt="news1">
+                        <div class="news-content">
+                            <h5>${items.title}</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pretium ex ac urna tempor, ac ultrices justo aliquet. Aenean viverra urna eu est tincidunt venenatis. </p>
+                            <div class="more-button">
+                                <button>
+                                    <a href="#">More</a>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+    `;
+                }).join(" ");
+    let a = document.querySelector(".news-container");
+    a.innerHTML = html;          
+};
+
 handleMenuTab1();
 handleMenuTab2();
 handleMenuTab3();
+handleNews();
